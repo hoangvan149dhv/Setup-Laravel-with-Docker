fcd98631e8d022f4e8a02cfdec10b3ac84e03c3e
Hoang Van <duong.hoang.van@sun-asterisk.com>
2022-03-06 16:19:05 +0700

    Set Up Laravel, Nginx, and MySQL With Docker Compose

--- www/laravel.sh
+++ www/laravel.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# Create env
+cp ./.env.example ./.env
+
 # composer install
 composer install
 
+# Generate key
+php artisan key:generate
 # Remove cache
 php artisan cache:clear
 php artisan view:clear
